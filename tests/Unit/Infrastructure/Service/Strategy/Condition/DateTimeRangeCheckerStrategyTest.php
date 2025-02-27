f893406407d10eaebe6ca3a31eaa4039f22caf05
Georgiy Grobovoy <gro-jorge@mail.ru>
2025-02-27 17:12:07 +0300

    Added tests for some services

--- tests/Unit/Infrastructure/Service/Strategy/Condition/DateTimeRangeCheckerStrategyTest.php
+++ tests/Unit/Infrastructure/Service/Strategy/Condition/DateTimeRangeCheckerStrategyTest.php
@@ -11,26 +11,34 @@ use App\Application\Service\Registry\RoutingStepClassRegistryInterface;
 use App\Application\Service\Registry\RoutingStepSchemeClassRegistryInterface;
 use App\Domain\Entity\RoutingStepInterface;
 use App\Infrastructure\Service\Strategy\Condition\DateTimeRangeCheckerStrategy;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use DateTimeImmutable;
 
 final class DateTimeRangeCheckerStrategyTest extends TestCase
 {
-    public function testWhenInDateRange(): void
-    {
-        $onPassRoutingStepMock = $this->createMock(RoutingStepInterface::class);
+    #[DataProvider('getTestCases')]
+    public function testConditionChecking(
+        DateTimeImmutable $from, 
+        DateTimeImmutable $to, 
+        DateTimeImmutable $requestTime, 
+        bool $meetsCondition
+    ): void {
+        $nextRoutingStepMock = $this->createMock(RoutingStepInterface::class);
+
+        $methodToGetNextStep = true === $meetsCondition ? 'getOnPassStep' : 'getOnDeclineStep';
 
         $routingStepMock = $this->createMock(RoutingStepInterface::class);
         $routingStepMock->expects($this->once())
-                                ->method('getOnPassStep')
-                                ->willReturn($onPassRoutingStepMock);
+                                ->method($methodToGetNextStep)
+                                ->willReturn($nextRoutingStepMock);
 
         $httpRequestDto = new HttpRequestDto(
             '',
             '',
             [],
-            new DateTimeImmutable('2025-01-10 12:00:00'),
+            $requestTime,
         );
 
         $routingStepClassRegistryMock = $this->createMock(RoutingStepClassRegistryInterface::class);
@@ -44,8 +52,8 @@ final class DateTimeRangeCheckerStrategyTest extends TestCase
                                 ->willReturn(DateTimeRangeConditionScheme::class);
 
         $scheme = new DateTimeRangeConditionScheme();
-        $scheme->from = new DateTimeImmutable('2024-02-12 12:00:00');
-        $scheme->to = new DateTimeImmutable('2025-02-02 12:40:00');
+        $scheme->from = $from;
+        $scheme->to = $to;
 
         $denormalizerMock = $this->createMock(DenormalizerInterface::class);
         $denormalizerMock->expects($this->once())
@@ -60,6 +68,50 @@ final class DateTimeRangeCheckerStrategyTest extends TestCase
 
         $result = $strategy->doHandleRoutingStep($routingStepMock, $httpRequestDto, $this->createMock(RedirectionContextInterface::class));
 
-        $this->assertEqualsCanonicalizing($onPassRoutingStepMock, $result->getNextStep());
+        $this->assertEqualsCanonicalizing($nextRoutingStepMock, $result->getNextStep());
+    }
+
+    /**
+     * @return array<string, array{
+     *   from: DateTimeImmutable, 
+     *   to: DateTimeImmutable,
+     *   requestTime: DateTimeImmutable,
+     *   meetsCondition: bool
+     * }>
+     */
+    public static function getTestCases(): array
+    {
+        return [
+            'in_the_middle_of_range' => [
+                'from' => new DateTimeImmutable('2024-02-12 12:00:00'),
+                'to' => new DateTimeImmutable('2025-02-02 12:40:00'),
+                'requestTime' => new DateTimeImmutable('2025-01-10 12:00:00'),
+                'meetsCondition' => true,
+            ],
+            'before_range' => [
+                'from' => new DateTimeImmutable('2024-02-12 12:00:00'),
+                'to' => new DateTimeImmutable('2025-02-02 12:40:00'),
+                'requestTime' => new DateTimeImmutable('2023-01-10 12:00:00'),
+                'meetsCondition' => false,
+            ],      
+            'after_range' => [
+                'from' => new DateTimeImmutable('2024-02-12 12:00:00'),
+                'to' => new DateTimeImmutable('2025-02-02 12:40:00'),
+                'requestTime' => new DateTimeImmutable('2026-01-10 12:00:00'),
+                'meetsCondition' => false,
+            ],   
+            'at_the_beginning_of_range' => [
+                'from' => new DateTimeImmutable('2024-02-12 12:00:00'),
+                'to' => new DateTimeImmutable('2025-02-02 12:40:00'),
+                'requestTime' => new DateTimeImmutable('2024-02-12 12:00:00'),
+                'meetsCondition' => true,
+            ],
+            'at_the_end_of_range' => [
+                'from' => new DateTimeImmutable('2024-02-12 12:00:00'),
+                'to' => new DateTimeImmutable('2025-02-02 12:40:00'),
+                'requestTime' => new DateTimeImmutable('2025-02-02 12:40:00'),
+                'meetsCondition' => false,
+            ],                                  
+        ];
     }
 }
