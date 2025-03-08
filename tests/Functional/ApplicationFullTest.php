2e854c716bc78bb873fd0e0e43558d9285571bdb
Georgiy Grobovoy <gro-jorge@mail.ru>
2025-03-08 16:13:55 +0300

    upd api method test

--- tests/Functional/ApplicationFullTest.php
+++ tests/Functional/ApplicationFullTest.php
@@ -45,7 +45,7 @@ final class ApplicationFullTest extends WebTestCase
 
         $this->client->jsonRequest('POST', '/api/v1/route', $createRouteRequestData);
 
-        $this->assertResponseStatusCodeSame(200);
+        $this->assertResponseStatusCodeSame(201);
 
         $this->client->request('GET', '/test/hello/posts');
 
@@ -54,6 +54,83 @@ final class ApplicationFullTest extends WebTestCase
         $this->client->request('GET', '/unknown_route');
 
         $this->assertResponseStatusCodeSame(404);  
+    }
+
+    public function testCreateRouteAndThanUpdate(): void
+    {
+        $createRouteRequestData = [
+            'urlPattern' => '/test/{foo_param}/posts',
+            'priority' => 1,
+            'isActive' => true,
+            'initialStep' => [
+                'type' => 'condition',
+                'schemeType' => 'datetime_range',
+                'schemeProps' => [
+                    'from' => date('Y-m-d H:i:s', time() - 86400),
+                    'to' => date('Y-m-d H:i:s', time() + 86400),
+                ],
+                'onPassStep' => [
+                    'type' => 'redirect',
+                    'schemeType' => 'redirect',
+                    'schemeProps' => [
+                        'url' => 'redirect1.com/page/{foo_param}',
+                    ],
+                ],
+                'onDeclineStep' => [
+                    'type' => 'redirect',
+                    'schemeType' => 'redirect',
+                    'schemeProps' => [
+                        'url' => 'redirect1.com/page/{foo_param}',
+                    ],
+                ],
+            ],
+        ];
+
+        $this->client->jsonRequest('POST', '/api/v1/route', $createRouteRequestData);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertJson($this->client->getResponse()->getContent());
+
+        $responseData = json_decode($this->client->getResponse()->getContent(), true);
+        
+        $this->assertArrayHasKey('id', $responseData);
+
+        $routeId = $responseData['id'];
+
+        $this->assertIsInt($routeId);
+
+        $this->assertGreaterThan(0, $routeId);
+
+        $updateRouteRequestData = [
+            'urlPattern' => '/test/{foo_param}/posts',
+            'priority' => 2,
+            'isActive' => true,
+            'initialStep' => [
+                'type' => 'condition',
+                'schemeType' => 'week_day',
+                'schemeProps' => [
+                    'weekDays' => [1, 2],
+                ],
+                'onPassStep' => [
+                    'type' => 'redirect',
+                    'schemeType' => 'redirect',
+                    'schemeProps' => [
+                        'url' => 'redirect2.com/page/{foo_param}',
+                    ],
+                ],
+                'onDeclineStep' => [
+                    'type' => 'redirect',
+                    'schemeType' => 'redirect',
+                    'schemeProps' => [
+                        'url' => 'redirect2.com/page/{foo_param}',
+                    ],
+                ],
+            ],
+        ];
+
+        $this->client->jsonRequest('PUT', '/api/v1/route/' . $routeId, $updateRouteRequestData);
+
+        $this->assertResponseStatusCodeSame(200);
     }
 
     protected function setUp(): void
